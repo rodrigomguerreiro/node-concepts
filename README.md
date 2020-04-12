4c7f7f02941ff549788fe2b6a83a5cfe702c3a12
Rodrigo Guerreiro <rodrigomguerreiro@outlook.com>
2020-04-12 15:58:09 -0400

    node-concepts

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center">
+    <img alt="Gostack11.0" title="#gostack11" src="./github/gostack11.png"/>
+</h1>
+
+<h4 align="center">
+  🚀 Node.Js Concepts Challenge
+</h4>
+
+<p align="center">
+
+<a href="https://nodejs.org/en/">
+  <img alt="node badge" src="https://img.shields.io/badge/node-v12.16.1-brightgreen">
+</a>
+
+</p>
+
+<br>
+
+## 🚀 Technologies
+
+- [Node.js](https://nodejs.org/en/)
+
+## 🤔 How to Install
+
+- Clone this repository `https://github.com/rodrigomguerreiro/node-concepts.git`;
+- Navegue até o diretório: `cd node-concepts/...`;
+- run: `yarn` to install dependencies;
+- run: `yarn test` to check validations;
+
+## Be happy.
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const findRepositoryIndex = repositories.findIndex(repository => 
+    repository.id === id
+  );
+
+    if(findRepositoryIndex === -1) {
+      return response.status(400).json({ error: 'Repository does not exists.' });
+    }
+
+    const repository = {
+      id,
+      title,
+      url,
+      techs,
+      likes: repositories[findRepositoryIndex].likes,
+    }
+
+    repositories[findRepositoryIndex] = repository;
+
+    return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository => 
+    repository.id === id
+  );
+
+    if(findRepositoryIndex >= 0) {
+      repositories.splice(findRepositoryIndex, 1);
+    } else {
+      return response.status(400).json({ error: 'Repository does not exist.' });
+    }
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository => 
+    repository.id === id
+  );
+
+  if(findRepositoryIndex === -1) {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  repositories[findRepositoryIndex].likes ++;
+
+  return response.json(repositories[findRepositoryIndex]);
+
 });
 
 module.exports = app;
